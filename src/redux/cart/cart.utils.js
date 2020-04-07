488e9601ca3b1a8bc8a0c550a8e02e20f00750d5
allan-viveiros <viveirosallan@gmail.com>
2020-04-06 21:06:52 -0400

    Added aadItem and removeItem functionality in the arrow buttons

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,7 @@
-
+/* Add items to cart. 
+ cartItems: is all items in my carts 
+ cartItemToAdd: is my new item
+*/
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
@@ -13,4 +16,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
+};
+
+/* Remove an unit from cart. 
+ cartItems: is all items in my carts 
+ cartItemToRemove: is my item to remove an unit
+*/
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem        
+    );
 };
